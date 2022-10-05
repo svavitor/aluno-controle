eb9fc310adaebc22a7cebd91834dd178d48f6090
Vitor Senna <vitor.twist@gmail.com>
2022-10-05 20:01:59 -0300

    First commit

---
+++ src/test/java/com/alunocontrole/acontrole/AcontroleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alunocontrole.acontrole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcontroleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
